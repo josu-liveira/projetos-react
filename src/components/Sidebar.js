9821dbc39b4293862995b0fd36356548bf036079
Josué Oliveira <167824520+josu-liveira@users.noreply.github.com>
2024-07-03 08:39:20 -0400

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Sidebar() {
-  
+
   return (
     <div className="sidebar">
       <div className="menu">
